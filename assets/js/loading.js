f78d9a85a583b10b0a4709c6f8f85db9321e3b1e
dev2 <gregorybouyssou>
2019-06-25 12:36:56 +0200

    3nd commit

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -2,23 +2,27 @@ function Select(mavar){
     return document.querySelector(mavar)
 }
 
-
 let pourcentage = Select(".load span")
 let skillbar = Select(".skill")
 let bouton = Select(".monbouton")
+let fisti = Select(".fisti")
+
+let countFisti = 130
+
+fisti.style.right = "-" + countFisti + "%"
 
 let count = 5
 let p = setInterval(function(){
     count++
+    countFisti--
     if(count === 100){
         clearInterval(p)
         bouton.style.display = "block"
     }
     pourcentage.innerHTML = count + '%'
     skillbar.style.width = count + '%'
-    console.log(count)
+    fisti.style.right = "-" + countFisti + "%"
 },1000/12)
 
 
 
-console.log(bouton)
